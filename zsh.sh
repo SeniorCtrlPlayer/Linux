ea0971da8286aafba6f5250527b58f6bb4955a0e
SeniorCtrlPlayer <510062390@qq.com>
2019-10-20 22:41:51 +0800

    zsh_cmd_config

---
+++ zsh.sh
@@ -0,0 +1,24 @@
+# 安装powerline字体
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+# 字体测试
+echo "\ue0b0 \u00b1 \ue0a0 \u27a6 \u2718 \u26a1 \u2699"
+
+# 安装zsh
+sudo yum install -y zsh
+
+# 配置zsh
+# 安装oh-my-zsh
+# sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+wget -c https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -0 install.sh
+# 修改主题配置文件
+cmd1="mv zshrc ~/.zshrc"
+sed -i "/	setup_zshrc/a \\$cmd1" install.sh
+# 屏蔽用户名及主机名
+cmd2="sed -i 's/  prompt_context/ #prompt_context/' ~/.oh-my-zsh/themes/agnoster.zsh-theme"
+sed -i "/	setup_ohmyzsh/a \\$cmd2" install.sh
+# 屏蔽用户名及主机名
+sh ./install.sh

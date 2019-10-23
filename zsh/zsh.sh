db3c800aeccfdcec49cbcc48c9a16583d18e2718
SeniorCtrlPlayer <38762187+SeniorCtrlPlayer@users.noreply.github.com>
2019-10-23 20:00:12 +0800

    Update zsh.sh
    zsh-config bug fix

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -13,11 +13,11 @@ sudo yum install -y zsh
 # 配置zsh
 # 安装oh-my-zsh
 # sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-wget -c https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh install.sh
+wget -c https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh ./install.sh
 # 修改主题配置文件
 cmd1="cp zshrc ~/.zshrc"
 sed -i "/	setup_zshrc/a \\$cmd1" install.sh
 # 屏蔽用户名及主机名
 cmd2="sed -i 's/  prompt_context/ #prompt_context/' ~/.oh-my-zsh/themes/agnoster.zsh-theme"
 sed -i "/	setup_ohmyzsh/a \\$cmd2" install.sh
-sh install.sh
+./install.sh
